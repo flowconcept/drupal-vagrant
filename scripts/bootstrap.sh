301fd14e2191775dcd8e668bf5ad7137de7afbf5
Volker Killesreiter <killesre@gmail.com>
2014-11-12 16:22:21 +0100

    install drush 7 via composer, removed d7 specific commands

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -38,9 +38,18 @@ if [ ! -e $BOOTSTRAPPED ]; then
   php5enmod uploadprogress
 
   # Drush
-  sudo pear channel-discover pear.drush.org
-  sudo pear install drush/drush-5.10.0.0
-  sudo drush --version
+  curl -sS https://getcomposer.org/installer | php
+  mv composer.phar /usr/local/bin/composer
+  ln -s /usr/local/bin/composer /usr/bin/composer
+
+  git clone https://github.com/drush-ops/drush.git /usr/local/src/drush
+  cd /usr/local/src/drush
+  git checkout 7.0.0-alpha5  #or whatever version you want.+
+  curl https://github.com/smk/drush/commit/b8fc591740a210b0affc04f85d720ed0db12ab5c.patch | patch
+  ln -s /usr/local/src/drush/drush /usr/bin/drush
+  composer install
+  composer update
+  drush --version
 
   # Solr
   #apt-get install -y solr-jetty
