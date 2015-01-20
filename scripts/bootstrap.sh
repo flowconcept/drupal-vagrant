a90cb382654facda314cd03b347b12ff6b1cbc0d
Stefan M. Kudwien <mail@stefankudwien.com>
2015-01-20 16:27:16 +0100

    Chasing drush HEAD.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -44,8 +44,7 @@ if [ ! -e $BOOTSTRAPPED ]; then
 
   git clone https://github.com/drush-ops/drush.git /usr/local/src/drush
   cd /usr/local/src/drush
-  git checkout 7.0.0-alpha5  #or whatever version you want.+
-  curl https://github.com/smk/drush/commit/b8fc591740a210b0affc04f85d720ed0db12ab5c.patch | patch
+  git checkout 7.0.0-alpha8  #or whatever version you want.+
   ln -s /usr/local/src/drush/drush /usr/bin/drush
   composer install
   composer update
