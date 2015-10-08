d50d792f3b4fe5e07c3b57434136d0a87f49ffe1
Volker Killesreiter <killesre@gmail.com>
2015-10-08 18:39:44 +0200

    use bento box debian 8.2

--- vagrant.rb
+++ vagrant.rb
@@ -9,8 +9,8 @@ check_plugins
 
 # Init $config hash with default values
 $config = {
-  box: "opscode-debian-8.1",
-  box_url: "http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_debian-8.1_chef-provisionerless.box",
+  box: "opscode-debian-8.2",
+  box_url: "http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_debian-8.2_chef-provisionerless.box",
   synced_folder_type: "nfs",
   memory: 1024,
   cpus: 1,
