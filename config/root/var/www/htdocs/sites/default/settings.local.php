bd05a548c6dba976868c19079979005a8553ccba
Stefan M. Kudwien <mail@stefankudwien.com>
2015-10-27 12:30:10 +0100

    Synced settings.local.php with development.settings.local.php

--- config/root/var/www/htdocs/sites/default/settings.local.php
+++ config/root/var/www/htdocs/sites/default/settings.local.php
@@ -1,7 +1,8 @@
 <?php
+
 /**
  * @file
- * Development configuration overrides.
+ * Local development override configuration feature.
  */
 
 /**
@@ -19,39 +20,28 @@ $databases['default']['default'] = array (
 );
 
 /**
- * configuration
+ * Assertions.
+ *
+ * The Drupal project primarily uses runtime assertions to enforce the
+ * expectations of the API by failing when incorrect calls are made by code
+ * under development.
+ *
+ * @see http://php.net/assert
+ * @see https://www.drupal.org/node/2492225
+ *
+ * If you are using PHP 7.0 it is strongly recommended that you set
+ * zend.assertions=1 in the PHP.ini file (It cannot be changed from .htaccess
+ * or runtime) on development machines and to 0 in production.
+ *
+ * @see https://wiki.php.net/rfc/expectations
  */
-#$config_directories['active'] = '';
-#$config_directories['staging'] = '';
+assert_options(ASSERT_ACTIVE, TRUE);
+\Drupal\Component\Assertion\Handle::register();
 
 /**
  * Enable local development services.
  */
-#$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
-
-/**
- * Allow test modules and themes to be installed.
- *
- * Drupal ignores test modules and themes by default for performance reasons.
- * During development it can be useful to install test extensions for debugging
- * purposes.
- */
-$settings['extension_discovery_scan_tests'] = TRUE;
-
-/**
- * Enable access to rebuild.php.
- *
- * This setting can be enabled to allow Drupal's php and database cached
- * storage to be cleared via the rebuild.php page. Access to this page can also
- * be gained by generating a query string from rebuild_token_calculator.sh and
- * using these parameters in a request to rebuild.php.
- */
-$settings['rebuild_access'] = FALSE;
-
-/**
- * Allow access to update.php script.
- */
-$settings['update_free_access'] = FALSE;
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
 
 /**
  * Override configuration values.
@@ -60,7 +50,9 @@ $settings['update_free_access'] = FALSE;
 
 /**
  * Show all error messages, with backtrace information.
- * Possible values: hide, some, all, verbose.
+ *
+ * In case the error level could not be fetched from the database, as for
+ * example the database connection failed, we rely only on this value.
  */
 $config['system.logging']['error_level'] = 'verbose';
 
@@ -71,14 +63,51 @@ $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
 
 /**
- * Disable the render cache.
+ * Disable the render cache (this includes the page cache).
+ *
+ * Note: you should test with the render cache enabled, to ensure the correct
+ * cacheability metadata is present. However, in the early stages of
+ * development, you may want to disable it.
  *
  * This setting disables the render cache by using the Null cache back-end
  * defined by the development.services.yml file above.
  *
  * Do not use this setting until after the site is installed.
  */
-#$settings['cache']['bins']['render'] = 'cache.backend.null';
+# $settings['cache']['bins']['render'] = 'cache.backend.null';
+
+/**
+ * Disable Dynamic Page Cache.
+ *
+ * Note: you should test with Dynamic Page Cache enabled, to ensure the correct
+ * cacheability metadata is present (and hence the expected behavior). However,
+ * in the early stages of development, you may want to disable it.
+ */
+# $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+/**
+ * Allow test modules and themes to be installed.
+ *
+ * Drupal ignores test modules and themes by default for performance reasons.
+ * During development it can be useful to install test extensions for debugging
+ * purposes.
+ */
+# $settings['extension_discovery_scan_tests'] = TRUE;
+
+/**
+ * Enable access to rebuild.php.
+ *
+ * This setting can be enabled to allow Drupal's php and database cached
+ * storage to be cleared via the rebuild.php page. Access to this page can also
+ * be gained by generating a query string from rebuild_token_calculator.sh and
+ * using these parameters in a request to rebuild.php.
+ */
+$settings['rebuild_access'] = TRUE;
+
+/**
+ * Allow access to update.php script.
+ */
+$settings['update_free_access'] = TRUE;
 
 // Limit log size.
 $config['dblog.settings']['row_limit'] = 1000;
@@ -86,6 +115,9 @@ $config['dblog.settings']['row_limit'] = 1000;
 // Adjust local file system paths.
 $settings['file_private_path'] = '/var/www/private';
 $config['system.file']['path']['temporary'] = '/tmp';
+
+// Trusted host configuration.
+# $settings['trusted_host_patterns'] = array('^www\.example\.de$');
 
 /**
  * Configure Stage File Proxy origin.
