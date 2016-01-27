fce467018ab227cc1cfa782b5c6eedd4f0b2f9f3
Stefan M. Kudwien <mail@stefankudwien.com>
2016-01-27 15:39:47 +0100

    Removes config.vbguest.auto_update since a version mismatch may cause high cpu load (see http://stackoverflow.com/a/28487054/480863)

--- vagrant.rb
+++ vagrant.rb
@@ -71,7 +71,6 @@ Vagrant.configure("2") do |config|
 
   # Sync VirtualBox guest additions (vagrant-vbguest)
   config.vbguest.no_remote = true
-  config.vbguest.auto_update = false
 
   # Fix running as tty
   config.ssh.pty = false
